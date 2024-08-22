121ac03a6c36b4c90f07b8a99372bf2468aaa7d8
idail <neto_br_8@hotmail.com>
2024-08-21 22:50:58 -0400

    Implementações
    Implementado a consulta de pedidos para gestor

--- api/autenticacao/autenticacao.php
+++ api/autenticacao/autenticacao.php
@@ -22,37 +22,47 @@ if($resultado_fornecedor)
 {
     $id = $resultado_fornecedor["id"];
     $nome_usuario = $resultado_fornecedor["name"];
+
+    $instrucaoBuscaGPFornecedor = "select grupo_pessoa_id from pessoa_grupo where pessoa_id = :recebe_pessoa_id";
+    $comandoBuscaGPFornecedor = Conexao::Obtem()->prepare($instrucaoBuscaGPFornecedor);
+    $comandoBuscaGPFornecedor->bindValue(":recebe_pessoa_id",$id);
+    $comandoBuscaGPFornecedor->execute();
+    $resultado_gpfornecedor = $comandoBuscaGPFornecedor->fetch(PDO::FETCH_ASSOC);
+
+    if($resultado_gpfornecedor)
+        $id_grupo_pessoafornecedor = $resultado_gpfornecedor["grupo_pessoa_id"];
+    else
+        $id_grupo_pessoafornecedor = "nao localizado";
+
+    $instrucaoBuscaGrupoFornecedor = "select nome from grupo_pessoa where id = :recebe_id_grupo_pessoa";
+    $comandoBuscaGrupoFornecedor = Conexao::Obtem()->prepare($instrucaoBuscaGrupoFornecedor);
+    $comandoBuscaGrupoFornecedor->bindValue(":recebe_id_grupo_pessoa",$id_grupo_pessoafornecedor);
+    $comandoBuscaGrupoFornecedor->execute();
+    $resultadoBuscaGrupoFornecedor = $comandoBuscaGrupoFornecedor->fetch(PDO::FETCH_ASSOC);
+
+    if($resultadoBuscaGrupoFornecedor)
+        $nome_grupo = $resultadoBuscaGrupoFornecedor["nome"];
+    else
+        $nome_grupo = "nao localizado";
+
+    $informacoes = ["nome" => $nome_usuario,"nome_grupo_usuario" => $nome_grupo, "codigo_usuario_autenticado" => $id];
 }
 else{
-    $id = "nao localizado";
+    $instrucaoBuscaGestor = "SELECT * FROM system_users where id not in(select system_user_id from pessoa) and login = :recebe_login_usuario and password = :recebe_senha_usuario";
+    $comandoBuscaGestor = Conexao::Obtem()->prepare($instrucaoBuscaGestor);
+    $comandoBuscaGestor->bindValue(":recebe_login_usuario",$recebeUsuario);
+    $comandoBuscaGestor->bindValue(":recebe_senha_usuario",$recebeSenhaCripografada);
+    $comandoBuscaGestor->execute();
+    $resultado_gestor = $comandoBuscaGestor->fetch(PDO::FETCH_ASSOC);
+
+    if($resultado_gestor)
+    {
+        $id = $resultado_gestor["id"];
+        $nome_usuario = $resultado_gestor["name"];
+        $informacoes = ["nome" => $nome_usuario,"nome_grupo_usuario" => "Gestor", "codigo_usuario_autenticado" => $id];
+    }
 }
-    
 
-
-$instrucaoBuscaGPFornecedor = "select grupo_pessoa_id from pessoa_grupo where pessoa_id = :recebe_pessoa_id";
-$comandoBuscaGPFornecedor = Conexao::Obtem()->prepare($instrucaoBuscaGPFornecedor);
-$comandoBuscaGPFornecedor->bindValue(":recebe_pessoa_id",$id);
-$comandoBuscaGPFornecedor->execute();
-$resultado_gpfornecedor = $comandoBuscaGPFornecedor->fetch(PDO::FETCH_ASSOC);
-
-if($resultado_gpfornecedor)
-    $id_grupo_pessoafornecedor = $resultado_gpfornecedor["grupo_pessoa_id"];
-else
-    $id_grupo_pessoafornecedor = "nao localizado";
-
-$instrucaoBuscaGrupoFornecedor = "select nome from grupo_pessoa where id = :recebe_id_grupo_pessoa";
-$comandoBuscaGrupoFornecedor = Conexao::Obtem()->prepare($instrucaoBuscaGrupoFornecedor);
-$comandoBuscaGrupoFornecedor->bindValue(":recebe_id_grupo_pessoa",$id_grupo_pessoafornecedor);
-$comandoBuscaGrupoFornecedor->execute();
-$resultadoBuscaGrupoFornecedor = $comandoBuscaGrupoFornecedor->fetch(PDO::FETCH_ASSOC);
-
-if($resultadoBuscaGrupoFornecedor)
-    $nome_grupo = $resultadoBuscaGrupoFornecedor["nome"];
-else
-    $nome_grupo = "nao localizado";
-
-$informacoes = ["nome" => $nome_usuario,"nome_grupo_usuario" => $nome_grupo, "codigo_usuario_autenticado" => $id];
-
-
+if(!empty($informacoes))
 echo json_encode($informacoes);
 ?>
--- api/pedidos/busca_pedidos.php
+++ api/pedidos/busca_pedidos.php
@@ -1,10 +1,16 @@
 <?php
 require("../Conexao.php");
 $codigo = $_GET["codigo_usuario"];
-$instrucaoBuscaPedidos = "select * from pedido as p where p.system_users_id = :recebe_codigo_usuario GROUP BY descricaopedido";
-$comandoBuscaPedidos = Conexao::Obtem()->prepare($instrucaoBuscaPedidos);
-$comandoBuscaPedidos->bindValue(":recebe_codigo_usuario",$codigo);
-$comandoBuscaPedidos->execute();
-$resultado = $comandoBuscaPedidos->fetchAll(PDO::FETCH_ASSOC);
+$tipo_acesso = $_GET["tipo_acesso"];
+
+if($tipo_acesso === "Gestor")
+{
+    $instrucaoBuscaPedidosGestor = "SELECT DISTINCT p.id,p.dt_pedido,p.descricaopedido,ep.nome,p.valor_total,p.valor_total_cotacao FROM system_user_departamento_unit as su inner join
+     pedido as p on su.departamento_unit_id = p.departamento_unit_id inner join estado_pedido as ep on p.estado_pedido_venda_id = ep.id where p.system_users_id = :recebe_codigo_usuario and ep.id = 13;";
+    $comandoBuscaPedidosGestor = Conexao::Obtem()->prepare($instrucaoBuscaPedidosGestor);
+    $comandoBuscaPedidosGestor->bindValue(":recebe_codigo_usuario",$codigo);
+    $comandoBuscaPedidosGestor->execute();
+    $resultado = $comandoBuscaPedidosGestor->fetchAll(PDO::FETCH_ASSOC);
+}
 echo json_encode($resultado);
 ?>
