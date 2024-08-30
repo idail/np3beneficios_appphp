a50a66b784a62af15dfca6984415c00f573bf788
idail <neto_br_8@hotmail.com>
2024-08-30 11:37:57 -0300

    Implementações
    Sendo ajustado as cores do aplicativo

--- api/mapa/localizacao.php
+++ api/mapa/localizacao.php
@@ -6,7 +6,9 @@ $codigo_usuario = $_GET["codigo_usuario"];
 
 if($perfil === "gestor")
 {
-    $instrucaoBuscaEnderecoGestor = "select su.rua,su.numero,c.nome,e.nome from system_unit as su inner join cidade as c on su.cidade_id = c.id inner join estado as e on c.estado_id = e.id where su.id = :recebe_codigo_usuario";
+    $instrucaoBuscaEnderecoGestor = "select sunit.rua,sunit.numero,c.nome,e.nome from system_unit as sunit inner join 
+    system_users as su on sunit.id = su.system_unit_id inner join cidade as c on sunit.cidade_id = c.id
+     inner join estado as e on c.estado_id = e.id where su.id = :recebe_codigo_usuario";
     $comandoBuscaEnderecoGestor = Conexao::Obtem()->prepare($instrucaoBuscaEnderecoGestor);
     $comandoBuscaEnderecoGestor->bindValue(":recebe_codigo_usuario",$codigo_usuario);
     $comandoBuscaEnderecoGestor->execute();
@@ -15,8 +17,9 @@ if($perfil === "gestor")
     if($resultadoBuscaEnderecoGestor)
         echo json_encode($resultadoBuscaEnderecoGestor);
 }else{
-    $instrucaoBuscaEnderecoFornecedor = "select pe.rua,pe.numero,c.nome,e.nome from system_users as su inner join pessoa as p on p.system_users = su.id inner join pessoa_endereco as pe on pe.pessoa_id = p.id
-    inner join cidade as c on pe.cidade_id = c.id inner join estado as e on c.estado_id = e.id where su.id = :recebe_codigo_usuario";
+    $instrucaoBuscaEnderecoFornecedor = "select pend.rua,pend.numero,c.nome,e.nome from system_users as su inner join
+     pessoa as pe on su.id = pe.system_user_id inner join pessoa_endereco as pend on pe.id = pend.pessoa_id inner join
+      cidade as c on pend.cidade_id = c.id inner join estado as e on c.estado_id = e.id where su.id = :recebe_codigo_usuario";
     $comandoBuscaEnderecoFornecedor = Conexao::Obtem()->prepare($instrucaoBuscaEnderecoFornecedor);
     $comandoBuscaEnderecoFornecedor->bindValue(":recebe_codigo_usuario",$codigo_usuario);
     $comandoBuscaEnderecoFornecedor->execute();
